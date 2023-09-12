e65d5afb8505c01e1ca084d2558e28a9133410b4
husone <sonhvnde160056@fpt.edu.vn>
2023-09-12 10:43:20 +0700

    slot1

---
+++ common_components/New.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import { Text } from "react-native";
+
+class New extends Component{
+    render(){
+        return (
+            <Text>Hello, React Native</Text>
+        );
+    }
+}
