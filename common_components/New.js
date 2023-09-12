84c665d70e91a103f52c1f98ceecc023f898dd38
husone <sonhvnde160056@fpt.edu.vn>
2023-09-12 10:47:15 +0700

    Slide 25

--- common_components/New.js
+++ common_components/New.js
@@ -1,12 +1,10 @@
 import React, {Component} from "react";
 import { Text } from "react-native";
 
-class New extends Component{
-    render(){
-        return (
-            <Text>Hello, React Native</Text>
-        );
-    }
+const New = () => {
+    return (
+        <Text>Hello, React Native</Text>
+    );
 }
 
 export default New;
